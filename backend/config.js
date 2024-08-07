abf25e6725bf7b70874b5a84eaef90b3e9d03448
Mallikarjunayya S H <mallikhiremath123@gmail.com>
2024-08-07 19:33:25 +0530

    updates
    updates

--- backend/config.js
+++ backend/config.js
@@ -5,5 +5,5 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const PORT = process.env.PORT || 5555;
+export const PORT = process.env.PORT;
 export const mongoDBURL = process.env.MONGODB_URI;
